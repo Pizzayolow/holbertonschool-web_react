f44ca3f6f837f9f3be765a19ab25a1c552f2f043
Valentin RALLIARD <vralliard@ccld.com>
2025-01-28 10:38:10 +0100

    modify dashboard.js

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,22 @@
-// Importer jQuery
-import $ from 'jquery';
+import $ from "jquery";
+import { debounce } from "lodash";
 
-// Ajouter les éléments HTML dans l'ordre demandé
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+let counter = 0;
 
 
-export function updateCounter() {
-    let count = parseInt($('count'.text()));
-    count += 1;
-
-    $('#count').text(count);
+// Function increment counter
+function updateCounter() {
+  counter += 1;
+  $('#count').text(`${counter} clicks on the button`);
 }
 
-$('button').click(updateCounter);
+
+// Funciton who generate HTML.
+$(function () {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+  $('button').on('click', _.debounce(updateCounter, 500));
+});
