0022196db19c69d4743e91428dd54761f2e724fc
Valentin RALLIARD <vralliard@ccld.com>
2025-02-21 15:22:51 +0100

    task 0 finally ????

--- react_intro/task_3/dashboard/src/setupTests.js
+++
@@ -1,8 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-18';
-import { TextEncoder, TextDecoder } from 'util';
-
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
-
-configure({ adapter: new Adapter() });
--- react_intro/task_5/dashboard/config/setupTests.js
+++
@@ -1,8 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import 'fast-text-encoding';
-
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
-
-configure({ adapter: new Adapter() });
