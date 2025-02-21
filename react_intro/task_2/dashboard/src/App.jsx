40d679d5a412a339a525994dc80a76cc108888b4
Valentin RALLIARD <vralliard@ccld.com>
2025-02-21 11:11:27 +0100

    task2

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -1,32 +1,27 @@
-import React from "react";
-import logo from "./holberton_logo.jpg";
-import "./App.css";
-import { getFooterCopy, getFullYear } from "./utils";
+import logo from './holberton_logo.jpg';
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
 
 function App() {
   return (
     <>
       <div className="App-header">
-        <img src={logo} alt="Holberton Logo"></img>
+        <img src={logo} alt='Holberton Logo'></img>
         <p>School Dashboard</p>
       </div>
-      <div className="App-body">
+      <div className='App-body'>
         <p>Login to access the full dashboard</p>
-        <form>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" />
-
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
-
-          <button type="submit">OK</button>
-        </form>
+        <label htmlFor="email">Email: </label>
+        <input type='email' id="email"></input>
+        <label htmlFor="password">Password: </label>
+        <input type='password' id='password'></input>
+        <input type="submit" value='OK'></input>
       </div>
-      <div className="App-footer">
+      <div className='App-footer'>
         <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
       </div>
     </>
-  );
+  )
 }
 
 export default App;
