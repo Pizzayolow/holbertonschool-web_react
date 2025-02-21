2c95626d3c0ec021e55c10856c1903316e8b595d
Valentin RALLIARD <vralliard@ccld.com>
2025-02-21 11:04:26 +0100

    update App

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -1,14 +1,22 @@
-import React from "react";
-import { getCurrentYear, getFooterCopy } from "./utils";
+import logo from './holberton_logo.jpg';
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-footer">
-        <p>© {getCurrentYear()} {getFooterCopy(true)}</p>
+    <>
+      <div className="App-header">
+        <img src={logo} alt='Holberton Logo'></img>
+        <p>School Dashboard</p>
       </div>
-    </div>
-  );
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className='App-footer'>
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </div>
+    </>
+  )
 }
 
 export default App;
