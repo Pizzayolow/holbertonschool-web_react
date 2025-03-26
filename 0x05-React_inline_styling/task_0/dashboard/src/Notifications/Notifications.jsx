4bbf32fbfa12bae21849d79781a37155af0d8e14
Valentin RALLIARD <vralliard@ccld.com>
2025-03-26 10:10:07 +0100

    task 4

--- 0x05-React_inline_styling/task_0/dashboard/src/Notifications/Notifications.jsx
+++ 0x05-React_inline_styling/task_0/dashboard/src/Notifications/Notifications.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./Notifications.css";
+import { StyleSheet, css, keyframes } from "aphrodite";
 import closeImage from "../assets/close-button.png";
 import NotificationItem from "./NotificationItem";
 
@@ -11,12 +11,7 @@ class Notifications extends React.Component {
   }
 
   shouldComponentUpdate(nextProps) {
-    if (
-      nextProps.notificationsList.length !== this.props.notificationsList.length
-    )
-      return true;
-
-    return false;
+    return nextProps.notificationsList.length !== this.props.notificationsList.length;
   }
 
   markAsRead(id) {
@@ -25,11 +20,14 @@ class Notifications extends React.Component {
 
   render() {
     const { notificationsList, displayDrawer } = this.props;
+
     return (
       <>
-        <div>Your notifications</div>
-        {displayDrawer ? (
-          <div className="notifications">
+        {!displayDrawer && (
+          <div className={css(styles.menuItem)}>Your notifications</div>
+        )}
+        {displayDrawer && (
+          <div className={css(styles.notifications)}>
             {notificationsList.length === 0 ? (
               <p>No new notification for now</p>
             ) : (
@@ -38,22 +36,15 @@ class Notifications extends React.Component {
                 <button
                   aria-label="Close"
                   onClick={() => console.log("Close button has been clicked")}
-                  style={{
-                    position: "absolute",
-                    top: "10px",
-                    right: "10px",
-                    background: "none",
-                    border: "none",
-                    cursor: "pointer",
-                  }}
+                  className={css(styles.closeButton)}
                 >
                   <img
                     src={closeImage}
                     alt="Close icon"
-                    style={{ width: "10px", height: "10px" }}
+                    className={css(styles.closeIcon)}
                   />
                 </button>
-                <ul>
+                <ul className={css(styles.list)}>
                   {notificationsList.map((notif) => (
                     <NotificationItem
                       key={notif.id}
@@ -67,13 +58,67 @@ class Notifications extends React.Component {
               </>
             )}
           </div>
-        ) : (
-          ""
         )}
       </>
     );
   }
 }
+
+const fadeIn = keyframes({
+  from: { opacity: 0.5 },
+  to: { opacity: 1 },
+});
+
+const bounce = keyframes({
+  "0%": { transform: "translateY(0px)" },
+  "50%": { transform: "translateY(-5px)" },
+  "100%": { transform: "translateY(5px)" },
+});
+
+const styles = StyleSheet.create({
+  menuItem: {
+    float: "right",
+    backgroundColor: "#fff8f8",
+    padding: "10px",
+    fontWeight: "bold",
+    cursor: "pointer",
+    ":hover": {
+      animationName: [fadeIn, bounce],
+      animationDuration: "1s, 0.5s",
+      animationIterationCount: "3",
+    },
+  },
+  notifications: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "white",
+    padding: "2rem",
+    fontSize: "20px",
+    zIndex: 999,
+    overflowY: "auto",
+    boxSizing: "border-box",
+    border: "none",
+  },
+  closeButton: {
+    position: "absolute",
+    top: "10px",
+    right: "10px",
+    background: "none",
+    border: "none",
+    cursor: "pointer",
+  },
+  closeIcon: {
+    width: "10px",
+    height: "10px",
+  },
+  list: {
+    padding: 0,
+    listStyleType: "none",
+  },
+});
 
 Notifications.propTypes = {
   notificationsList: PropTypes.arrayOf(PropTypes.object),
